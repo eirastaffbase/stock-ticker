229bb767cc2cd731f12a46f15ab07c189199aac6
eirastaffbase <eira.tope@staffbase.com>
2025-03-06 15:43:14 -0500

    init

---
+++ src/stock-ticker.test.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import {screen, render} from "@testing-library/react"
+
+import {StockTicker} from "./stock-ticker";
+
+describe("StockTicker", () => {
+    it("should render the component", () => {
+        render(<StockTicker contentLanguage="en_US" message="World"/>);
+
+        expect(screen.getByText(/Hello World/)).toBeInTheDocument();
+    })
+})
